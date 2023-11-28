a6f7ff136a400c4cbbc9bbe8deea49367c10ad74
Feruza Makhmudova <latipova.feruza12@gmail.com>
2023-11-28 18:28:33 +0500

    [wip]

---
+++ app/components/Header/__tests__/Header.test.tsx
@@ -0,0 +1,15 @@
+
+import { render, screen } from '@testing-library/react'
+import Header from '../Header';
+describe('Header', () => {
+
+    it('should render the "Next Todos" heading', () => {
+        render(<Header title="Next Todos" />)
+
+        const header = screen.getByRole('heading', {
+            name: 'Next Todos'
+        })
+
+        expect(header).toBeInTheDocument()
+    })
+});
---
+++ app/components/TodoItem/__tests__/TodoItem.test.tsx
@@ -0,0 +1,7 @@
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import TodoItem from '../TodoItem';
+
+const mockTodo={
+    
+}
